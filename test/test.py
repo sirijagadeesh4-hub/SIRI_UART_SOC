4e8477730215e3666329526bd33e7642f23ad10b
sirijagadeesh4-hub <sirijagadeesh4@gmail.com>
2026-05-26 15:00:04 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,40 +1,38 @@
-# SPDX-FileCopyrightText: © 2024 Tiny Tapeout
-# SPDX-License-Identifier: Apache-2.0
-
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import ClockCycles
+from cocotb.triggers import RisingEdge
 
 
 @cocotb.test()
 async def test_project(dut):
-    dut._log.info("Start")
 
-    # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, unit="us")
+    # Start clock
+    clock = Clock(dut.clk, 10, units="us")
     cocotb.start_soon(clock.start())
 
     # Reset
-    dut._log.info("Reset")
-    dut.ena.value = 1
-    dut.ui_in.value = 0
-    dut.uio_in.value = 0
     dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
+    dut.ui_in.value = 0
+
+    for _ in range(2):
+        await RisingEdge(dut.clk)
+
     dut.rst_n.value = 1
 
-    dut._log.info("Test project behavior")
+    # Send 7-bit UART data = 1010101
+    bits = [1, 0, 1, 0, 1, 0, 1]
 
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
+    for bit in bits:
+        dut.ui_in.value = bit
+        await RisingEdge(dut.clk)
 
-    # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
+    # Wait one extra cycle
+    await RisingEdge(dut.clk)
 
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+    # Check done signal
+    assert dut.uio_out.value[0] == 1
 
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+    # Check received data
+    assert dut.uo_out.value != 0
+
+    cocotb.log.info("UART test passed")
